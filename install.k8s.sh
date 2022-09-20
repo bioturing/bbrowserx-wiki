b79a85866eaad77255a69e09eae96042d2c57744
Paul Pham <thaihoabo@gmail.com>
2022-09-20 18:16:42 +0700

    Fix k8s install

--- install.k8s.sh
+++ install.k8s.sh
@@ -5,7 +5,7 @@ set -e
 echo "Your K8S engines [vanilla, microk8s]: "
 read K8S_DIST
 if [ -z "$K8S_DIST" ] || [ "$K8S_DIST" != "microk8s" ]; then
-    bash ./k8s/microk8s.install.sh
-else
     bash ./k8s/vanillak8s.install.sh
+else
+    bash ./k8s/microk8s.install.sh
 fi
